1047fd2b7a82f5345af1d9bae75aa14d32898564
Damian Galli <damian.galli@galli.site>
2021-05-14 16:08:49 +0200

    Inital Commit

---
+++ README.md
@@ -0,0 +1,11 @@
+# Companion Script for Nginx-Proxy-Manager
+
+Some applications need direct access to ssl certs because they serve other protocols then https (e.x Matrix/LDAP etc)
+
+This script syncs the certs to an other server so that you can acces the certs directly
+
+
+* Auth your local ssh key to target server
+* Edit sync-ssh.bash script for your server
+* Install systemctl task
+* Start systemctl task
---
+++ sync-ssh.bash
@@ -0,0 +1,20 @@
+#!/bin/bash
+#This script syncs the ssl keys down to the proxied server
+#start this script as systemctl process
+#use included systemctl file
+# apt install inotify-tools
+CERT_PATH_HOST="/root/proxy-manager/letsencrypt/live/" # path of the certs on the current machine,should end with wildcard or file 
+SSH_KEYFILE="/root/.ssh/id_rsa" # key that is authed to target server
+SSH_TARGET="root@192.168.178.81:/ssl/cert" # target sever:path (rsync)
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
+rm -rf $SCRIPT_DIR/tmp-sync
+while inotifywait -r $CERT_PATH_HOST; do
+    mkdir $SCRIPT_DIR/tmp-sync
+    rsync -avL $CERT_PATH_HOST tmp-sync/ # Resolve Symlinks
+    cd tmp-sync
+    mv npm-11 wildcard.galli.site
+    mv npm-13 galli.site
+    cd ..
+    rsync -av --delete -e "ssh -i $SSH_KEYFILE" $SCRIPT_DIR/tmp-sync/ $SSH_TARGET
+    rm -rf $SCRIPT_DIR/tmp-sync
+done
